e26590592e657c54fab18569e905f2aa3c25578f
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-03-28 15:30:35 +0100

    implement functionality

--- src/main/java/com/zayzou/SayHello.java
+++ src/main/java/com/zayzou/SayHello.java
@@ -1,9 +1,19 @@
 package com.zayzou;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 public class SayHello {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        var language = args[0];
+        InputStream ressource = SayHello.class.getResourceAsStream(language);
+        assert ressource != null;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(ressource, StandardCharsets.UTF_8));
+        System.out.println(bufferedReader.readLine());
     }
 }

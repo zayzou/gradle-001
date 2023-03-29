3ed7332694589c3971fa90f276660f8619bb9185
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-03-29 11:51:58 +0100

    Create a Test for sayHello

---
+++ src/test/java/com/zayzou/SayHelloTest.java
@@ -0,0 +1,14 @@
+package com.zayzou;
+
+
+import org.junit.jupiter.api.Test;
+
+public class SayHelloTest {
+
+
+
+    @Test
+    public void testSayHello() {
+        SayHello.main(new String[]{"en"});
+    }
+}
